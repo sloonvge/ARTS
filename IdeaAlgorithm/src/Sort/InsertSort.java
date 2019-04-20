7b35c696163114604ba065c9b469c6911afba9be
wanjx <wjx2699@outlook.com>
2019-04-20 10:05:13 +0800

    第五周

--- IdeaAlgorithm/src/Sort/InsertSort.java
+++ IdeaAlgorithm/src/Sort/InsertSort.java
@@ -5,5 +5,32 @@ package Sort;
  */
 
 public class InsertSort {
+    public void sort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            for(int j = i; j > 0 && nums[j] < nums[j - 1]; j--) {
+                int tmp = nums[j];
+                nums[j] = nums[j-1];
+                nums[j-1] = tmp;
+            }
+        }
+    }
 
+    public void string(int[] a) {
+        if (a == null) {
+            return;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "\t");
+        }
+        System.out.print("\n");
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 12, 60, 1, 233, 3};
+        InsertSort S = new InsertSort();
+        // S.merge(a, 2);
+        S.string(a);
+        S.sort(a);
+        S.string(a);
+    }
 }
